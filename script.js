51b5c49023e45f606275374b25ede309578aa734
Sarah B <sarahbinaz1020@gmail>
2021-03-01 19:26:15 -0700

    Added JS

--- script.js
+++ script.js
@@ -1,5 +1,73 @@
 // Assignment Code
+
+// Retreive a reference to the button with an id of generate
 var generateBtn = document.querySelector("#generate");
+
+// Array of special characters to be included in password
+var specialCharacters = ["!", "@", "#", "$", "%", "&", "?" ]
+
+// Array of numeric characters to be included in password
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
+// Array of uppercase characters to be included in password
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+
+// Array of lowercase characters to be included in password
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+
+//Prompt user for password length
+var userPassLength = prompt("Pick a password length between 8 and 128 characters"); 
+
+// if (userPassLength >8 && userPassLength <128) {}
+
+// Using a confirm prompt the user for special characters
+
+var userSpecial = confirm ("Do you want special characters?");
+
+// // Using a confirm prompt the user for numeric characters
+
+var userNumeric = confirm ("Do you want numbers?" );
+
+// // Using a confirm prompt the user for uppercase characters
+
+var userUppercase = confirm ("Do you want uppercase values?");
+
+// // Using a confirm prompt the user for lowercase characters
+
+var userLowercase = confirm ("Do you want lowercase values?");
+
+//set up the array
+var bigArrayOfPossibilities = [ ];
+
+if (userSpecial === true){
+  // add stuff from an array to a different array
+  bigArrayOfPossibilities.push(userSpecial);
+  } 
+if (userNumeric === true) {
+  bigArrayOfPossibilities.push(userNumeric);
+  } 
+if (userUppercase === true) {
+  bigArrayOfPossibilities.push(userUppercase);
+  } 
+if (userLowercase === true) {
+  bigArrayOfPossibilities.push(userLowercase);
+  }
+//Algo for password generation goes below
+
+var getRandomItem = bigArrayOfPossibilities[Math.floor(Math.random() * bigArrayOfPossibilities.length)];
+console.log(getRandomItem);
+// run 
+function getRandomItem() {
+for(let i = 0; i < userPassLength.length; i++); 
+return password.slice(0, userPassLength);
+} 
+
+//return the build password
+// return "Hello I am the password placeholder :)";   
+
+// return "Password should go here";
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +76,18 @@ function writePassword() {
 
   passwordText.value = password;
 
+// function generatePassword(a,b,c, d) {
+//   // uses the User choices to actually generate passwrod
+//   // var fullstring = a + b + c + d
+
+
+// }
+
+// gerneratePassword("cat", "hat", "bat", "mat");
+// //this is going to return  a string "cathatbatmat"
+
+
+
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
